05d0920597d009c39d5aa2a1776327724b306a8b
Mehmet Akif Koru <mehmetakifkoru@outlook.com>
2025-02-22 20:48:03 +0300

    general lines are completed

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username' : 'Akif'}
+	posts = [
+		{
+			'author' : {'username' : 'egemen'},
+			'body' : 'Sennn Cidddi misiiiinn?'
+		},
+		{
+			'author' : {'username' : 'gazoz'},
+			'body' : 'Fısssss!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts = posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # ...
+        return redirect(url_for('index'))
 
